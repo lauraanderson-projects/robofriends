e4e5b117ad262ebf7d565898b6dccb13363db216
sphirebluela <laura.sphireblue@gmail.com>
2025-07-18 13:29:50 -0700

    Changes to card call
    added search box to main screen

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,15 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ id, name, email }) => {
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img
-        alt="robots"
-        src={`https://robohash.org/123456?set=set2&size=200x200`}
-      />
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+      <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
       <div>
-        <h2>Robot Name</h2>
-        <p>Robot Description</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
